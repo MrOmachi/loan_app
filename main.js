60f3a47ea633f9e1ff9c49bd88803c4232a6ace9
MrOmachi <omachi.john@yahoo.com>
2022-03-20 18:33:28 +0100

    This is a loan app clone

---
+++ main.js
@@ -0,0 +1,101 @@
+let accountBalance = 50000;
+let customerPoints = 0;
+let regBtn = document.getElementById('regBtn');
+let lastCredited = document.getElementById('lastCredited');
+let lastDeposit = document.getElementById('lastDeposit');
+let lastLoanCollected = document.getElementById('lastLoanCollected');
+let loanRepayment = document.getElementById('loanRepayment');
+let accountType = document.getElementById('accountType');
+let amountError = document.getElementById('amountError');
+
+regBtn.addEventListener('click', function () {
+  lastTimeCredited();
+});
+
+function lastTimeCredited() {
+  if (lastCredited.value == 'withingSixMonth') {
+    customerPoints = customerPoints + 10;
+    console.log('you got 10 point');
+  } else if (lastCredited.value == 'aboveSevenMonth') {
+    customerPoints = customerPoints + 0;
+    console.log('you got no point here');
+  }
+  lastTimedeposited();
+}
+
+function lastTimedeposited() {
+  if (lastDeposit.value == 'sixMonthDeposit') {
+    customerPoints = customerPoints + 5;
+    console.log('you got 5 points');
+  } else if (lastDeposit.value == 'sevenMonthDeposit') {
+    customerPoints == customerPoints + 0;
+    console.log('you got no point here');
+  }
+  lastLoanTaken();
+}
+
+function lastLoanTaken() {
+  if (lastLoanCollected.value == 'sixMonthLoan') {
+    customerPoints = customerPoints + 0;
+    console.log('you got no point');
+  } else if (lastLoanCollected.value == 'sevenMonthLoan') {
+    customerPoints = customerPoints + 10;
+    console.log('you got 10 points');
+  }
+  loanPay();
+}
+
+function loanPay() {
+  if (loanRepayment.value == 'repaySixMonth') {
+    customerPoints = customerPoints + 5;
+    console.log('you got 5 points');
+  } else if (loanRepayment.value == 'repayAboveMonth') {
+    customerPoints = customerPoints + 0;
+    console.log('you got no points');
+  }
+  customerAccountType();
+}
+
+function customerAccountType() {
+  if (accountType.value == 'current') {
+    customerPoints = customerPoints + 10;
+    console.log('you got 10 points');
+  } else if (accountType.value == 'savings') {
+    customerPoints = customerPoints + 5;
+    console.log('you got 5 points');
+  }
+  amountOfLoan();
+}
+
+function amountOfLoan() {
+  if (loanAmount.value == '') {
+    amountError.innerHTML = 'Loan amount is required!';
+    customerPoints = customerPoints + 0;
+
+    console.log('you got no points');
+  } else if (loanAmount.value > accountBalance) {
+    amountError.innerHTML = '';
+    customerPoints = customerPoints - 10;
+    console.log('you got -10 points');
+  } else if (loanAmount.value <= accountBalance) {
+    amountError.innerHTML = '';
+    customerPoints = customerPoints + 10;
+    console.log('you got 10 points');
+  }
+  requiementChech();
+}
+
+function requiementChech() {
+  if (customerPoints >= 30) {
+    alert(
+      'Viola!!! you are eligible for this loan and your application is successful'
+    );
+  } else if (customerPoints < 30 && customerPoints > 0) {
+    alert('Oga abeg getat');
+  }
+  validatePoints();
+}
+
+function validatePoints() {
+  console.log('Customer cummulative points is ' + customerPoints);
+}
